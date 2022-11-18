0aaac209beb261a0f30dca366905c2ccbee63014
Anna Schmidt <marshall1697@gmail.com>
2022-11-18 08:14:07 -0700

    added modal component to confirm delete review

--- client/src/components/ReviewsFolder/ReviewPlantsCard.js
+++ client/src/components/ReviewsFolder/ReviewPlantsCard.js
@@ -1,24 +1,35 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import { useState } from 'react'
+import {Link} from "react-router-dom"
+import ModalComponent from '../ModalComponent'
 
-const ReviewPlantsCard = ({ comment, id, setUpdateReviews, image, setUpdateAfterDelete }) => {
+const ReviewPlantsCard = ({ comment, id, image, setUpdateAfterDelete }) => {
+
+  const [openModal, setOpenModal] = useState(false)
 
   function handleDelete(){
     fetch(`/reviews/${id}`, {
       method: 'DELETE',
     })
     .then(setUpdateAfterDelete)
-
   }
+
+    //open/close modal
+    const handleOpen = () => setOpenModal(true);
+    const handleClose = () => setOpenModal(false);
   
   return (
     <div className="review-card plant-card">
       <img src={image} alt={id} />
       <h3>{comment}</h3>
-      <button className= "delete-btn" onClick={handleDelete} >X</button>
+
       <Link to={`/reviews/${id}`}>
       <button className= "edit-btn">Edit</button>
       </Link>
+
+      <button className= "delete-btn" onClick={handleOpen}>X</button>
+
+      <ModalComponent openModal={openModal} handleDelete={handleDelete} handleClose={handleClose}/>
+
     </div>
   )
 }
