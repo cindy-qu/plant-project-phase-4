353c196a9fb77ffc0fa53763eb724285633f9c59
Anna Schmidt <marshall1697@gmail.com>
2022-11-18 20:42:43 -0700

    responsive navbar and media queries

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
+import { useState } from 'react'
+import { NavLink, useHistory } from 'react-router-dom'
+
 
 const NavBar = ({ user, setUser }) => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
 
     const history = useHistory()
 
@@ -18,13 +20,40 @@ const NavBar = ({ user, setUser }) => {
 
     return (
         <div className='nav'>
+             <button className='hamburger' onClick={() => {
+              setIsNavExpanded(!isNavExpanded)
+             }}>
+              <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="white"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+             </button>
+
+          <div className={isNavExpanded ? 'nav-menu expanded' : 'nav-menu'}>
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/myPlants">My Plants</NavLink>
-            <NavLink to="/reviews">My Reviews</NavLink>
-            <NavLink to="/globalPlants">Global Plants</NavLink>
+             <NavLink to="/about">About</NavLink>
+             <NavLink to="/myPlants">My Plants</NavLink>
+              <NavLink to="/reviews">My Reviews</NavLink>
+             <NavLink to="/globalPlants">Global Plants</NavLink>
+            {/* <ul> */}
+              {/* <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+              <li><NavLink to="/myPlants">My Plants</NavLink></li>
+              <li> <NavLink to="/reviews">My Reviews</NavLink></li>
+              <li><NavLink to="/globalPlants">Global Plants</NavLink></li> */}
+            {/* </ul> */}
+            </div>
             <span>Hello {user.username}!
             <button className='sign-out' onClick={handleLogoutClick}>Logout</button>
+           
             </span>
         </div>
 
